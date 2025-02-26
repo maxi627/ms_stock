09f7f4c91bab756159d4b37951954f610608b925
maxi627 <eulamaxi@gmail.com>
2025-02-26 14:25:51 -0300

    ..

--- app/mapping/stock_schema.py
+++ app/mapping/stock_schema.py
@@ -3,9 +3,9 @@ from app.models import Stock
 
 class StockSchema(Schema):
     id = fields.Integer(dump_only=True)  # Solo se devuelve, no se requiere para crear
-    producto_id = fields.Integer(required=True)  # Cambiado a producto_id para coincidir con el modelo
-    fecha_transaccion = fields.DateTime(required=True)  # Marcado como requerido
-    cantidad = fields.Float(required=True)  # Marcado como requerido
+    producto_id = fields.Integer(required=True)  
+    fecha_transaccion = fields.DateTime(required=True) 
+    cantidad = fields.Float(required=True)  
     entrada_salida = fields.Integer(
         required=True, validate=validate.OneOf([1, 2])  # Validación para entrada/salida
     )
--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
 
 por sí no toma el módulo app
-set PYTHONPATH=C:\Users\Usuario Digital\OneDrive\Documentos\3ro Ing. En Sistemas\ms_compra
+set PYTHONPATH=
+
